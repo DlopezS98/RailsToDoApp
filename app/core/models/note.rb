3b94e0c223893304025015924cd4307a6a722ab2
Danny Lopez <dalopez@inss.gob.ni>
2021-11-15 15:55:15 -0600

    map relationship between notes and users

--- app/core/models/note.rb
+++ app/core/models/note.rb
@@ -1,2 +1,6 @@
 class Note < ApplicationRecord
+    belongs_to :users, 
+        class_name: 'user', 
+        foreign_key: 'user_id', 
+        primary_key: 'id'
 end
--- app/core/models/user.rb
+++ app/core/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  self.table_name = 'users'
+
+  has_many :notes, 
+    class_name: 'note', 
+    foreign_key: 'user_id', 
+    primary_key: 'id'
 end
