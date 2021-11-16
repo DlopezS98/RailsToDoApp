3d5e133286ed95581cee7fde8500e501eed6f083
DlopezS98 <01dlopezs98@gmail.com>
2021-11-15 23:58:25 -0600

    get notes by user

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,7 +4,7 @@ class NotesController < ApplicationController
 
   # GET /notes or /notes.json
   def index
-    @notes = Note.all
+    @notes = Note.where(user_id: current_user.id)
   end
 
   # GET /notes/1 or /notes/1.json
