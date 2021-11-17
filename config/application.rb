08109f8c5ad711ed84a2b07126410199c8428561
DlopezS98 <01dlopezs98@gmail.com>
2021-11-17 11:00:08 -0600

    rspec rails setup

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,15 @@ module RailsToDoApp
     config.eager_load_paths << Rails.root.join("app/core/models")
 
     config.middleware.delete(ActiveRecord::Migration::CheckPending)
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: true
+      # g.fixture_replacement :factory_bot, dir: 'spec/factories/'
+    end
   end
 end
