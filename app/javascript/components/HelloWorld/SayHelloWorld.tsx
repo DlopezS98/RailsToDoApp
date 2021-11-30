b475da55ba6a982031569d1b09202e1052b0b0ae
DlopezS98 <01dlopezs98@gmail.com>
2021-11-30 08:26:38 -0600

    testing nested component in the same carpet

---
+++ app/javascript/components/HelloWorld/SayHelloWorld.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+class SayHelloWorld extends React.Component<IProps> {
+
+  constructor(props: IProps){
+    super(props);
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+          {this.props.greeting}, New component
+      </React.Fragment>
+    );
+  }
+}
+
+interface IProps {
+  greeting: String;
+}
+
+export default SayHelloWorld
